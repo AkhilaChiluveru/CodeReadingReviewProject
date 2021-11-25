fb691d64b4ed1dba320c3f130d5eca80612d741d
Srija Gaddam <srija@pdx.edu>
2021-11-25 13:04:42 -0800

    cleaning up app.js file

--- src/App.js
+++ src/App.js
@@ -6,15 +6,10 @@ import './App.css';
 import MainNews from './Components/News/MainNews'
 import LaunchPage from './Components/LaunchPage/Launchpage';
 import Events from "./Components/Events/events";
-import dictionaryicon from './Logos/dictionary-icon.png'
+
 import Display from "./Components/DisplayEvent";
 import { eventsConfig } from "./config";
-import newsicon  from './Logos/news-icon.png'
-import gifsicon from './Logos/GIfs-icon.gif'
-import musicicon from './Logos/music-icon.png'
-import quoteicon from './Logos/quote-icon.jpeg'
-import adviceicon from './Logos/advice-icon.png'
-import eventsicon from './Logos/events-icon.png'
+
 
 function App() {
   const [isEvent, setIsEvent] = useState(false);
@@ -26,13 +21,7 @@ function App() {
     );
   };
   const apiResponseHandler = (url, eventName) => {
-    //let headers = {};
-    /* if(eventName === 'gifs'){
-      headers ={
-        method:'GET',
-        headers:{"api_key":'frr4Czt15JJUC0jn5Vyl5Rgp8Dg9IHSO'}
-      }
-    } */
+    
     fetch(url)
       .then((res) => res.json())
       .then((data) => {
@@ -54,14 +43,9 @@ function App() {
           })}
       </div>
    </div>
-   <div className="output">
+     <div className="output">
           {displayData && <Display displayContent={displayData} />}
     </div>
-      <Routes>
-          <Route exact path='#' element={<LaunchPage />}></Route>         
-          <Route exact path='/news' element={<MainNews />}></Route>
-         
-        </Routes>
 </BrowserRouter>
    
   );
@@ -69,12 +53,3 @@ function App() {
 export default App;
 
 
-/*
- <Route exact path='/joke' element={<Joke />}></Route>
-          <Route exact path='/advice' element={<Joke />}></Route>
-          
-          <Route exact path='/quotes' element={<Joke />}></Route>
-          <Route exact path='/dictionary' element={<Joke />}></Route>
-          <Route exact path='/music' element={<Joke />}></Route>
-            <Route exact path='/gifs' element={<GIFs />}></Route>
-*/
