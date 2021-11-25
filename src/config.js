e248c220386e94cb5ab4482b960f72b4f655f0a3
Srija Gaddam <srija@pdx.edu>
2021-11-25 12:30:08 -0800

    Created event component for rendering icons

---
+++ src/config.js
@@ -0,0 +1,7 @@
+import jokeicon from "./Logos/joke-icon.jpeg";
+export const eventsConfig = [
+  {
+    name: "Jokes",
+    iconUrl: jokeicon,
+    apiUrl: "https://geek-jokes.sameerkumar.website/api?format=json",
+  }];
